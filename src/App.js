73483e12cf8d2602c966cd6448279bf249fd5a8d
Megh Shah <meghshah1710@gmail.com>
2019-09-09 17:34:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <Navbar />
             <div className="container">
               <Switch>
-                <Route exact path="https://megh98.github.io/LyricFinder/" component={Index} />
+                <Route exact path="/" component={Index} />
                 <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
